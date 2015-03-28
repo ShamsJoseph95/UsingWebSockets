fc49cda3e6ea06c4c2a06ae37e1a665c61daee19
Joseph Shams <shams.joseph95@gmail.com>
2015-03-28 17:17:01 +0000

    UsingWebSockets
    FIrst Attempy

---
+++ WebSockets/MicrosoftWebSockets.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Script.Serialization;
+using Microsoft.Web.WebSockets;
+using Newtonsoft.Json;
+
+namespace UsingWebSockets
+{
+    public class MicrosoftWebSockets : WebSocketHandler
+    {
+        private static WebSocketCollection clients = new WebSocketCollection();
+
+        public override void OnOpen()
+        { 
+            //const string names = "<div ng-init=\"friends = [{name:\'John\'},{name:\'Mary\'},{name:\'Mike\'},{name:\'Adam\'},{name:\'Julie\'},{name:\'Juliette\'}]\"></div>";
+            const string names = "<div ng-init='friends = [{name:'Yashar'}]'></div>";
+            clients.Add(this);
+            clients.Broadcast("connected.<br/><hr/>");
+            string allNames = JsonConvert.SerializeObject(names);
+            clients.Broadcast(allNames);                  
+            /********************************************************************/
+            //Dictionary<string, string> someNames = new Dictionary<string, string>();
+
+            //someNames.Add("name", "Yashar, Maral, Negin, Siamak");
+            //string js = JsonConvert.SerializeObject(someNames);
+            //clients.Broadcast(js);
+        }
+     
+        public override void OnClose()
+        {
+            clients.Remove(this);
+            clients.Broadcast(string.Format("Bye!"));
+        }
+
+    }
+}
